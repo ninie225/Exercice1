bcb8cddb9e4eb86c1810f8c1da2f984f4c3d4c7b
Marine <marin@DESKTOP-OMUQSJT>
2023-03-08 13:25:21 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom;
+            string prenom;
+            int age;
+            Console.Write("Quel est votre nom de famille ?");
+            nom = Console.ReadLine();
+            Console.Write("Quelle est votre prénom?");
+            prenom = Console.ReadLine();
+            Console.Write("Quel est votre âge?");
+            age=int.Parse(Console.ReadLine());
+            Console.WriteLine("Bonjour "+prenom+" "+nom+", vous avez "+age+" ans.");
+            Console.ReadLine();
+        }
+    }
+}
